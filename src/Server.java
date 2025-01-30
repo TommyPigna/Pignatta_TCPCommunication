046be2840ac65a8f14d2d3e54b4f639a24380455
TommyPigna <tommasopignatta@gmail.com>
2025-01-30 11:15:30 +0100

    Inizializzazione del progetto

---
+++ src/Server.java
@@ -0,0 +1,22 @@
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tommaso pignatta
+ */
+public class Server {
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    int porta;
+    
+    public Server(){
+    }
+}
